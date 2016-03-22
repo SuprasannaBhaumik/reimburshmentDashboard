26200e8a7075d4a0dedab13dd0154427c52713ae
SuprasannaBhaumik <suprasanna.bhaumik86@gmail.com>
2016-03-22 13:36:28 +0530

    added functions

--- custom.js
+++ custom.js
@@ -1,8 +1,32 @@
 var myApp = angular.module("myModule",[]);
 
 
-myApp.controller("myController",function($scope){
+/*myApp.controller("myController",function($scope){
 	$scope.message = "my Controller scope message property";
 });
+*/
 
-
+myApp.controller("PanelController",function($scope){
+	
+	this.tab=1;
+	this.selectTab = function(selectedTab){
+		this.tab = selectedTab;
+	}
+	this.isSelected=function(tabInQues){
+		return this.tab === tabInQues;
+	}
+	
+	this.expander=false;
+	this.symbol="+";
+	this.expanderClicked=function(){
+		if(this.expander === true){
+			this.expander=false;
+			this.symbol="+";
+		}else if(this.expander===false){
+			this.expander=true;
+			this.symbol="-";
+		}
+	};
+	
+	
+});
