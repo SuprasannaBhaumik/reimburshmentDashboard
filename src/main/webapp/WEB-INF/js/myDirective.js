d155e446e0895a450eef5cfa876694d5f9a134f3
SuprasannaBhaumik <suprasanna.bhaumik86@gmail.com>
2016-03-31 14:26:02 +0530

    added new custom directives for request form

--- src/main/webapp/WEB-INF/js/myDirective.js
+++ src/main/webapp/WEB-INF/js/myDirective.js
@@ -18,4 +18,72 @@ angular.module('myDirectives',[]).directive('myHeader',function(){
 		replace:false,
 		template:"<p align=right>{{userName}} | <a href=javascript:formSubmit()> Logout</a></p>"
 	}
+}).directive('claimRequestorname', function() {
+	return{
+		scope:{},
+		restrict:'E',
+		link:function(scope,e,a){
+		},
+		template:"<tr><td><b>Requestor Name</b> : <input type=text ng-model=reqName/></td></tr>"	
+	}
+}).directive('claimTelephone', function() {
+	return{
+		scope:{},
+		restrict:'E',
+		link:function(scope,e,a){
+			//scope.renderValue=a.heading;
+		},
+		template:"<tr><td><b>Requestor Telephone</b> : <input type=text ng-model=reqTel/></td></tr>"	
+	}
+	
+}).directive('claimRequestoremail', function() {
+	return{
+		scope:{},
+		restrict:'E',
+		link:function(scope,e,a){
+			//scope.renderValue=a.heading;
+		},
+		template:"<tr><td><b>Requestor Email</b> : <input type=text ng-model=reqEmail/></td></tr>"	
+	}
+	
+}).directive('claimManageremail', function() {
+	return{
+		scope:{},
+		restrict:'E',
+		link:function(scope,e,a){
+			//scope.renderValue=a.heading;
+		},
+		template:"<tr><td><b>Manager Email</b> : <input type=text ng-model=reqManagerEmail/></td></tr>"	
+	}
+	
+}).directive('claimId', function() {
+	return{
+		scope:{},
+		restrict:'E',
+		link:function(scope,e,a){
+			//scope.renderValue=a.heading;
+		},
+		template:"<tr><td><b>Claim Id</b> : <input type=text ng-model=reqId/></td></tr>"	
+	}
+	
+}).directive('claimAmount', function() {
+	return{
+		scope:{},
+		restrict:'E',
+		link:function(scope,e,a){
+			//scope.renderValue=a.heading;
+		},
+		template:"<tr><td><b>Claim Amount</b> : <input type=text ng-model=reqAmt/></td></tr>"	
+	}
+	
+}).directive('claimDate', function() {
+	return{
+		scope:{},
+		restrict:'E',
+		link:function(scope,e,a){
+			//scope.renderValue=a.heading;
+		},
+		template:"<tr><td><b>Request Date</b> : <input type=text ng-model=reqDt/></td></tr>"	
+	}
+	
 });
